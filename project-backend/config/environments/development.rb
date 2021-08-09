49a204e8c23f2aae504390e02bf6d60620dcd4ab
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-08-09 12:50:42 +0900

    Add posts api

--- project-backend/config/environments/development.rb
+++ project-backend/config/environments/development.rb
@@ -61,6 +61,10 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << 'localhost'
+  config.hosts << '127.0.0.1'
+  config.hosts << ENV['BACKEND_HOST']
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
--- project-backend/db/schema.rb
+++ project-backend/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_09_033130) do
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
